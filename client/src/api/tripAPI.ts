51098faab0fa35d30e7a2370111c5bc0a514f033
SpeedRelativity <necharkc5150@gmail.com>
2025-08-07 18:33:28 -0700

    new console log debug

--- client/src/api/tripAPI.ts
+++ client/src/api/tripAPI.ts
@@ -1,7 +1,7 @@
 // used to make requests to the backend
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 const saveTrip = async (trip: {
     title: string;
